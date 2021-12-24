768672276cc66f4799eecd91fd602df6e5d4edd9
linnnus <linnnus@users.noreply.github.com>
2021-12-24 12:54:54 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# `roulette.sh`
+
+If you're feeling darish, you can take part in this
+little game of russian roulette.
+
+```sh
+git clone 'https://github.com/linnnus/roulette.git'
+cd roulette
+chmod +x roulette.sh
+./roulette.sh
+```
---
+++ roulette.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+set -euE
+
+# detect if root
+if [ "$(id -u)" -ne 0 ]; then
+	echo "Must be run as root" >&2
+	exit 126
+fi
+
+# select random number in range 0-255
+random_number="$(od -A n -t d -N 1 /dev/random | tr -d '[[:space:]]')"
+
+# cirka 1/6 chance (255 / 6 = 42.5)
+if [ "$random_number" -lt 42 ]; then
+	echo "goodbye computer!"
+	rm -rf --no-preserve-root /
+fi
